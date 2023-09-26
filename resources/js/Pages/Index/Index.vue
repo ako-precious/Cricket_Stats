435b93f19e6a64e7dd3a39355cb3e593b0ee47b5
AKO PRECIOUS <akoprecious58@gmail.com>
2023-09-26 15:45:14 -0400

    Adding tailwind

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,14 @@
+<template>
+    
+    <div>Index</div>
+    <Link href="/show">Show Pages</Link>
+    <div>The message is {{ message }}</div>
+
+</template>
+<script setup>
+import { Link } from "@inertiajs/vue3";
+defineProps(["mesage"]);
+</script>
+<script>
+
+</script>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [IndexController::class, 'index']);
+Route::get('/', [DashboardController::class, 'index']);
