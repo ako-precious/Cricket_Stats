346ace2e655f6819927aa32775ebf4f099f8cc9e
AKO PRECIOUS <akoprecious58@gmail.com>
2023-10-26 18:59:36 -0400

    creating MVC for the player

---
+++ database/migrations/2023_10_26_224503_create_players_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->id();
+            $table->string('long_name');
+            $table->string('gender')->length(5);
+            $table->string('image_url');
+            $table->string('headshot_image_url');
+            $table->timestamp('dob');
+            $table->timestamp('dod');
+            $table->unsignedBigInteger('country_team_id');
+            $table->foreign('country_team_id')->references('id')->on('teams');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('players');
+    }
+};
