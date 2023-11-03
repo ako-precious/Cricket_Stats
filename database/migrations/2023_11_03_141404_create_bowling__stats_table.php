f9f2c885d659031abe6873e9ee231801015aa5d9
AKO PRECIOUS <akoprecious58@gmail.com>
2023-11-03 10:35:38 -0400

    migration for bowling

--- database/migrations/2023_11_03_141404_create_bowling__stats_table.php
+++ database/migrations/2023_11_03_141404_create_bowling__stats_table.php
@@ -13,6 +13,20 @@ return new class extends Migration
     {
         Schema::create('bowling__stats', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('player_id');
+            $table->foreign('player_id')->references('id')->on('players');
+            $table->string('match-format');
+            $table->int('matches');
+            $table->int('innings');
+            $table->int('balls');
+            $table->int('runs');
+            $table->int('wickets');
+            $table->double('average');
+            $table->double('economy');
+            $table->double('strike_rate');
+            $table->int('four_wicket_hauls');
+            $table->int('five_wicket_hauls');
+            $table->int('ten_wicket_hauls');
             $table->timestamps();
         });
     }
