e3c7c730bd7322c2d13dd6688ab1dc2919af44b8
AKO PRECIOUS <akoprecious58@gmail.com>
2023-11-10 03:39:54 -0500

    vb

--- resources/js/Layout/Component/filter.vue
+++ resources/js/Layout/Component/filter.vue
@@ -1,43 +1,59 @@
 <template>
     <form>
-      <div class="mb-8 mt-4 flex flex-wrap gap-2">
-        <div class="flex flex-nowrap items-center">
-          <input
-            type="text" placeholder="Price from"
-            class="input-filter-l w-28"
-          />
-          <input
-            type="text" placeholder="Price to" 
-            class="input-filter-r w-28"
-          />
+        <div class="mb-8 mt-4 flex flex-wrap gap-2">
+            <div class="flex flex-nowrap items-center">
+                <input
+                    type="text"
+                    placeholder="Price from"
+                    class="input-filter-l w-28"
+                />
+                <input
+                    type="text"
+                    placeholder="Price to"
+                    class="input-filter-r w-28"
+                />
+            </div>
+
+            <div class="flex flex-nowrap items-center">
+                <select class="input-filter-l w-28">
+                    <option :value="null">Beds</option>
+                    <option v-for="n in 5" :key="n" :value="n">{{ n }}</option>
+                    <option>6+</option>
+                </select>
+                <select class="input-filter-r w-28">
+                    <option :value="null">Baths</option>
+                    <option v-for="n in 5" :key="n" :value="n">{{ n }}</option>
+                    <option>6+</option>
+                </select>
+            </div>
+
+            <div class="flex flex-nowrap items-center">
+                <div
+                    class="flex items-center border-2 py-2 px-3 rounded-2xl mb-4"
+                >
+                    
+                    <input
+                        class="pl-2 outline-none border-none"
+                        type="text"
+                        name=""
+                        id=""
+                        placeholder="Email Address"
+                    />
+                </div>
+                <input
+                    type="text"
+                    placeholder="Area from"
+                    class="input-filter-l w-28"
+                />
+                <input
+                    type="text"
+                    placeholder="Area to"
+                    class="input-filter-r w-28"
+                />
+            </div>
+
+            <button type="submit" class="btn-normal">Filter</button>
+            <button type="reset">Clear</button>
         </div>
-  
-        <div class="flex flex-nowrap items-center">
-          <select class="input-filter-l w-28">
-            <option :value="null">Beds</option>
-            <option v-for="n in 5" :key="n" :value="n">{{ n }}</option>
-            <option>6+</option>
-          </select>
-          <select class="input-filter-r w-28">
-            <option :value="null">Baths</option>
-            <option v-for="n in 5" :key="n" :value="n">{{ n }}</option>
-            <option>6+</option>
-          </select>
-        </div>
-  
-        <div class="flex flex-nowrap items-center">
-          <input
-            type="text" placeholder="Area from"
-            class="input-filter-l w-28"
-          />
-          <input
-            type="text" placeholder="Area to"
-            class="input-filter-r w-28"
-          />
-        </div>
-  
-        <button type="submit" class="btn-normal">Filter</button>
-        <button type="reset">Clear</button>
-      </div>
     </form>
-  </template>
+</template>
--- resources/js/Pages/Battings/Index.vue
+++ resources/js/Pages/Battings/Index.vue
@@ -1,6 +1,7 @@
 <template>
     <MainLayout>
         <TableContainer>
+            <Filters />
             <div
                 style="backdrop-filter: blur(20px)"
                 class="relative flex flex-col min-w-0 mb-6 break-words bg-white border-0 border-transparent border-solid shadow-xl dark:bg-slate-850 dark:shadow-dark-xl rounded-2xl bg-clip-border"
@@ -170,6 +171,8 @@
 <script setup>
 import { Link } from "@inertiajs/vue3";
 import MainLayout from "@/Layout/MainLayout.vue";
+
+import Filters from "@/Layout/Component/Filter.vue";
 import Pagination from "@/Layout/Component/Pagination.vue";
 import TdLayout from "@/Layout/Component/Table/TdLayout.vue";
 import TableContainer from "@/Layout/Component/Table/TableContainer.vue";
