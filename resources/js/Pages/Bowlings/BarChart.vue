2ee8c1fc3297bdaffb65e61d44a2891d3baf2466
AKO PRECIOUS <akoprecious58@gmail.com>
2023-11-14 00:09:09 -0500

    mm

--- resources/js/Pages/Bowlings/BarChart.vue
+++ resources/js/Pages/Bowlings/BarChart.vue
@@ -1,30 +1,86 @@
 <template>
-    <Bar
-      id="my-chart-id"
-      :options="chartOptions"
-      :data="chartData"
-    />
-  </template>
-  
-  <script>
-  import { Bar } from 'vue-chartjs'
-  import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
-  
-  ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
-  
-  export default {
-    name: 'BarChart',
-    components: { Bar },
-    data() {
-      return {
-        chartData: {
-          labels: [ 'January', 'February', 'March' ],
-          datasets: [ { data: [40, 20, 12] } ]
-        },
-        chartOptions: {
-          responsive: true
-        }
-      }
-    }
-  }
-  </script>
+    <div>
+        <canvas id="myChart" width="400" height="400"></canvas>
+    </div>
+</template>
+
+<script>
+import Chart from "chart.js/auto";
+import {
+    Chart as ChartJS,
+    Title,
+    Tooltip,
+    Legend,
+    BarElement,
+    CategoryScale,
+    LinearScale,
+} from "chart.js";
+
+export default {
+    mounted() {
+        const ctx = document.getElementById("myChart");
+
+        new Chart(ctx, {
+            type: "bar",
+            data: {
+                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                datasets: [
+                    {
+                        label: "# of Votes",
+                        data: [12, 19, 3, 5, 2, 3],
+                        borderWidth: 1,
+                    },
+                ],
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                    },
+                },
+            },
+        });
+    },
+
+    // ChartJS.register(
+    //     Title,
+    //     Tooltip,
+    //     Legend,
+    //     BarElement,
+    //     CategoryScale,
+    //     LinearScale
+    // );
+    //   new Chart(ctx, {
+    //     type: 'bar',
+    //     data: {
+    //       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    //       datasets: [{
+    //         label: '# of Votes',
+    //         data: [12, 19, 3, 5, 2, 3],
+    //         borderWidth: 1
+    //       }]
+    //     },
+    //     options: {
+    //       scales: {
+    //         y: {
+    //           beginAtZero: true
+    //         }
+    //       }
+    //     }
+    //   });
+
+    // name: "BarChart",
+    // components: { Bar },
+    // data() {
+    //     return {
+    //         chartData: {
+    //             labels: ["January", "February", "March"],
+    //             datasets: [{ data: [40, 20, 12] }],
+    //         },
+    //         chartOptions: {
+    //             responsive: true,
+    //         },
+    //     };
+    // },
+};
+</script>
--- resources/js/Pages/Bowlings/Show.vue
+++ resources/js/Pages/Bowlings/Show.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <BarChart></BarChart>
 </template>
 
@@ -9,4 +9,82 @@ export default {
   name: 'App',
   components: { BarChart }
 }
+</script> -->
+
+<template>
+  <div>
+      <canvas id="myChart" width="400" height="400"></canvas>
+  </div>
+</template>
+
+<script>
+import Chart from "chart.js/auto";
+
+export default {
+  mounted() {
+      const ctx = document.getElementById("myChart");
+
+      new Chart(ctx, {
+          type: "bar",
+          data: {
+              labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+              datasets: [
+                  {
+                      label: "# of Votes",
+                      data: [12, 19, 3, 5, 2, 3],
+                      borderWidth: 1,
+                  },
+              ],
+          },
+          options: {
+              scales: {
+                  y: {
+                      beginAtZero: true,
+                  },
+              },
+          },
+      });
+  },
+
+  // ChartJS.register(
+  //     Title,
+  //     Tooltip,
+  //     Legend,
+  //     BarElement,
+  //     CategoryScale,
+  //     LinearScale
+  // );
+  //   new Chart(ctx, {
+  //     type: 'bar',
+  //     data: {
+  //       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  //       datasets: [{
+  //         label: '# of Votes',
+  //         data: [12, 19, 3, 5, 2, 3],
+  //         borderWidth: 1
+  //       }]
+  //     },
+  //     options: {
+  //       scales: {
+  //         y: {
+  //           beginAtZero: true
+  //         }
+  //       }
+  //     }
+  //   });
+
+  // name: "BarChart",
+  // components: { Bar },
+  // data() {
+  //     return {
+  //         chartData: {
+  //             labels: ["January", "February", "March"],
+  //             datasets: [{ data: [40, 20, 12] }],
+  //         },
+  //         chartOptions: {
+  //             responsive: true,
+  //         },
+  //     };
+  // },
+};
 </script>
