a1f5909af1bfebf54def2f4e299a9bede3a448d6
AKO PRECIOUS <akoprecious58@gmail.com>
2023-11-16 07:53:26 -0500

    new day

--- resources/js/Layout/Component/Table/TableContainer.vue
+++ resources/js/Layout/Component/Table/TableContainer.vue
@@ -6,7 +6,7 @@
                     <a
                         href="javascript:;"
                         class="inline-block px-5 py-2.5 mb-4 text-sm font-bold leading-normal text-center text-white align-middle transition-all ease-in border-solid border border-white/75 rounded-lg shadow-md cursor-pointer hover:-translate-y-px active:opacity-85 hover:shadow-xs bg-white/10 hover:bg-transparent tracking-tight-rem bg-150 bg-x-25"
-                        >New order</a
+                        >Compare Stats</a
                     >
                 </div>
                 <div class="flex">
@@ -19,44 +19,7 @@
                         >
                             Filters
                         </a>
-                        <ul
-                            dropdown-menu=""
-                            class="z-100 min-w-44 text-sm shadow-3xl duration-250 transform-dropdown before:duration-350 before:font-awesome before:ease before:text-5.5 dark:bg-slate-850 pointer-events-none absolute top-1.5 right-2 left-auto m-0 mt-2 block origin-top cursor-pointer list-none rounded-lg border-0 border-solid border-transparent bg-white bg-clip-padding px-2 py-4 text-left text-slate-500 opacity-0 transition-all will-change-transform before:absolute before:top-0 before:right-5 before:left-auto before:z-40 before:text-white before:transition-all before:content-['\f0d8'] sm:-mr-6"
-                        >
-                            <li class="relative">
-                                <a
-                                    class="py-1.2 lg:ease clear-both block w-full whitespace-nowrap rounded-lg px-4 font-normal text-slate-500 transition-colors hover:bg-gray-200 hover:text-slate-700 focus:bg-gray-200 focus:text-slate-700 dark:text-white dark:hover:bg-gray-200/80 dark:hover:text-slate-700 lg:duration-300"
-                                    href="javascript:;"
-                                    >Status: Paid</a
-                                >
-                            </li>
-                            <li class="relative">
-                                <a
-                                    class="py-1.2 lg:ease clear-both block w-full whitespace-nowrap rounded-lg px-4 font-normal text-slate-500 transition-colors hover:bg-gray-200 hover:text-slate-700 focus:bg-gray-200 focus:text-slate-700 dark:text-white dark:hover:bg-gray-200/80 dark:hover:text-slate-700 lg:duration-300"
-                                    href="javascript:;"
-                                    >Status: Refunded</a
-                                >
-                            </li>
-                            <li class="relative">
-                                <a
-                                    class="py-1.2 lg:ease clear-both block w-full whitespace-nowrap rounded-lg px-4 font-normal text-slate-500 transition-colors hover:bg-gray-200 hover:text-slate-700 focus:bg-gray-200 focus:text-slate-700 dark:text-white dark:hover:bg-gray-200/80 dark:hover:text-slate-700 lg:duration-300"
-                                    href="javascript:;"
-                                    >Status: Canceled</a
-                                >
-                            </li>
-                            <li class="relative">
-                                <hr
-                                    class="h-px my-2 bg-gradient-to-r from-transparent via-black/40 to-transparent"
-                                />
-                            </li>
-                            <li class="relative">
-                                <a
-                                    class="py-1.2 text-danger lg:ease clear-both block w-full whitespace-nowrap rounded-lg px-4 font-normal text-red-600 transition-colors hover:bg-gray-200 focus:bg-gray-200 dark:hover:bg-gray-200/80 lg:duration-300"
-                                    href="javascript:;"
-                                    >Remove Filter</a
-                                >
-                            </li>
-                        </ul>
+                        
                     </div>
                     <button
                         data-type="csv"
