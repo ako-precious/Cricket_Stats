568c5b083586b21bc6671b1927bc72825ee3ad86
AKO PRECIOUS <akoprecious58@gmail.com>
2023-11-19 20:39:37 -0500

    v

--- resources/js/Pages/Index/BarChart.vue
+++ resources/js/Pages/Index/BarChart.vue
@@ -1,57 +1,51 @@
 <template>
-    <Line :data="data" :options="options" />
+  <div>
+    <Line :data="chartData" :options="options" />
+  </div>
 </template>
 
 <script lang="ts">
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
-} from 'chart.js'
-import { Line } from 'vue-chartjs'
-import * as chartConfig from './chartConfig.js'
-import axios from 'axios';
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
-)
+import { Chart as ChartJS,  CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from "chart.js";
+import { Line } from "vue-chartjs";
+import axios from "axios";
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
 export default {
-  name: 'App',
+  // extends: LineChart,
   components: {
-    Line
+    Line,
   },
-  data() {
-
-    return chartConfig
+  props: ["chartData", "options"],
+  watch: {
+    chartData() {
+      this.$data._chart.destroy(); // Destroy the existing chart
+      this.renderChart(this.chartData, this.options);
+    },
   },
- methods: {
-  fetchChartData() {
-    axios.get('/top-batting-players').then((response) => {
-      this.chartData = {
-        labels: response.data.map((item) => item.label),
-        datasets: [
-          {
-            label: 'Top 10 Batting Players',
-            backgroundColor: 'rgba(75,192,192,0.4)',
-            borderColor: 'rgba(75,192,192,1)',
-            borderWidth: 1,
-            data: response.data.map((item) => item.value),
-          },
-        ],
-      };
-    });
+  mounted() {
+    this.fetchChartData();
   },
-}
-}
+  methods: {
+    fetchChartData() {
+      axios.get("/top-batting-players").then((response) => {
+        this.chartData = {
+          labels: response.data.map((item) => item.label),
+          datasets: [
+            {
+              label: "Top 10 Batting Players",
+              backgroundColor: "rgba(75,192,192,0.4)",
+              borderColor: "rgba(75,192,192,1)",
+              borderWidth: 1,
+              data: response.data.map((item) => item.value),
+            },
+          ],
+        };
+        this.options = {
+          responsive: true,
+          maintainAspectRatio: false,
+        };
+      });
+    },
+  },
+};
 </script>
