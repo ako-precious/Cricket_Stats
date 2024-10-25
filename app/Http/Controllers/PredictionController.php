cd5f9b268f7202e0ee40d100067c260874e5ca2d
AKO PRECIOUS <akoprecious58@gmail.com>
2024-10-25 08:01:15 -0400

    add the use GuzzleHttp\Client;

--- app/Http/Controllers/PredictionController.php
+++ app/Http/Controllers/PredictionController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use GuzzleHttp\Client;
 class PredictionController extends Controller
 {
     public function predict(Request $request)
