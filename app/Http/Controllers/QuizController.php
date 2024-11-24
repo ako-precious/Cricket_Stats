e3e6462e2a750824220e7e7b031194aaba8eae83
AKO PRECIOUS <akoprecious58@gmail.com>
2024-11-24 00:07:29 -0500

    Calculate Overall Percentage

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -27,5 +27,21 @@ public function submitAnswer(Request $request)
 
     return response()->json(['success' => true, 'is_correct' => $isCorrect]);
 }
+public function getResults()
+{
+    $userId = auth()->id();
+
+    // Get all user test data
+    $totalQuestions = UserTest::where('user_id', $userId)->count();
+    $correctAnswers = UserTest::where('user_id', $userId)->where('is_correct', true)->count();
+
+    $percentage = $totalQuestions > 0 ? ($correctAnswers / $totalQuestions) * 100 : 0;
+
+    return response()->json([
+        'total_questions' => $totalQuestions,
+        'correct_answers' => $correctAnswers,
+        'percentage' => round($percentage, 2),
+    ]);
+}
 
 }
